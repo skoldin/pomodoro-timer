9b1f90150d33f51ddb3edfea15fa9a34805116db
Igor Skoldin <i.skoldin@gmail.com>
2018-11-24 14:23:00 +0300

    Clean up

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-// app.use('/css', express.static(path.join(__dirname, 'build')))
-
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
